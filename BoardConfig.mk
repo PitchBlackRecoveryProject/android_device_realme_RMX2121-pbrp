3d23b6b4877d335e0bdff0ae3c8e27cedccb4e23
imranpopz <imranpopz32@gmail.com>
2021-03-28 08:52:03 +0530

    Boardconfig.mk : use TW_NO_EXFAT_FUSE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,7 @@ TW_EXCLUDE_TWRPAPP := true
 TW_INCLUDE_LOGICAL := oppo_product oppo_engineering preload_common
 TW_SUPPORT_INPUT_1_2_HAPTICS := true
 TW_OZIP_DECRYPT_KEY := 0000
+TW_NO_EXFAT_FUSE := true
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
